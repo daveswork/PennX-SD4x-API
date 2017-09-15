f1e817e779f3272b07d257704bd9c0eb66c4dd7c
Daves-Mac <daveswork@users.noreply.github.com>
2017-09-15 12:48:54 -0400

    Added the 'findToy' and 'animalsYoungerThan' APIs.

--- index.js
+++ index.js
@@ -5,18 +5,72 @@ var Animal = require('./Animal.js');
 var Toy = require('./Toy.js');
 
 
-app.use('/', (req, res) => {
-	res.json({ msg : 'It works!' });
+
+app.use('/findToy', (req, res) =>{
+
+    var toyId = req.query.id;
+    Toy.findOne({id:toyId} , (err, toy) => {
+        if(err){
+            res.type('html').status(500);
+            res.send('Error: '+ err);
+        }
+        else if (!toy){
+            res.type('html').status(200);
+            res.send({});
+        }
+        else {
+            res.send(toy);
+        }
+    });
+    //res.send('You requested item: '+toyId);
+
+});
+
+app.use('/findAnimals', (req, res) =>{
+    
+    var aSpecies = req.query.species;
+    var aTrait = req.query.trait;
+    var gender = req.query.gender;
+    Animal.find({species:aSpecies});
+    
+
+});
+
+app.use('/animalsYoungerThan', (req, res) =>{
+    var aAge = req.query.age;
+
+    Animal.find({age:{$lt: aAge}}, (err, animal) =>{
+        if(err){
+            res.type('html').status(500);
+            res.send('Error: '+err);
+        }
+        else if(!animal){
+            res.type('html').status(200);
+            res.send({});
+        }
+        else {
+
+            var aLen = animal.length;
+            var aNames = []
+            console.log(aLen);
+            for(var i = 0; i < aLen; i++){
+                aNames.push(animal[i].name);
+            };
+
+            var animals = {count:aLen, names: aNames};
+
+            res.send(animals);
+        }
+    
     });
 
-
-app.use('/findToy', (req, res) =>{});
-
-app.use('/findAnimals', (req, res) =>{});
-
-app.use('/animalIsYoungerThan', (req, res) =>{});
+});
 
 app.use('/calculatePrice', (req, res) =>{});
+
+app.use(/*default*/ (req, res) => {
+	res.json({ msg : 'It works!' });
+    });
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
