1b066e958158d86669ec0907613976a3a75fdd2d
Daves-Mac <daveswork@users.noreply.github.com>
2017-09-21 10:41:31 -0400

    Added some notes for future me.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 Week 4
 HW6
 
-Some major refactoring of HW6 to make it more redable to future me. 
-
+Some major refactoring of HW6 to make it more readable to future me. 
--- index.js
+++ index.js
@@ -4,8 +4,16 @@ var app = express();
 var Animal = require('./Animal.js');
 var Toy = require('./Toy.js');
 
+/*
+To do:
+-Input validation, don't want someone slipping in potentially harmful queries.
+-Error handling for absent fields.
+*/
 
 
+/*
+Fairly straightforward, just check the id against the 'toy' collection.
+*/
 app.use('/findToy', (req, res) =>{
 
     var toyId = req.query.id;
@@ -22,11 +30,21 @@ app.use('/findToy', (req, res) =>{
             res.send(toy);
         }
     });
-    //res.send('You requested item: '+toyId);
-
 });
 
+/*
+A little more complex than the first API.
+Retrieve animals based on some characteristics.
+Return only the following fields as a list of objects:
+-name
+-species
+-breed
+-gender
+-age
+*/
+
 app.use('/findAnimals', (req, res) =>{
+  //Build the query string from the query parameters.
 
     var terms = {};
     if(req.query.species)
@@ -36,10 +54,12 @@ app.use('/findAnimals', (req, res) =>{
     if(req.query.trait)
         terms.traits= req.query.trait;
 
+    //Check to see if there are *any* query parameters.
     if(Object.keys(terms).length == 0 ){
         res.type('html').status(200);
         res.send({});
     }else{
+    //After retrieving the query object, return only the required fields.
     Animal.find(terms, (err, animals) =>{
         if(err){
             res.type('html').status(500);
@@ -69,6 +89,11 @@ app.use('/findAnimals', (req, res) =>{
 
 });
 
+
+/*
+Pretty straightforward query, retrieve a list of animals that are younger than
+the age specified. Return the number of animals and their names.
+*/
 app.use('/animalsYoungerThan', (req, res) =>{
     var aAge = req.query.age;
     Animal.find({age:{$lt: aAge}}, (err, animal) =>{
@@ -103,13 +128,26 @@ app.use('/animalsYoungerThan', (req, res) =>{
 
 });
 
+/*
+The BEAST!
+*/
+
 app.use('/calculatePrice', (req, res) =>{
+  /*
+  First, initialize the two main lists we're working with:
+  -idList: The toy ids from the query.
+  -qtyList: The quantity of toys.
+  */
     var idList =  []
     idList = req.query.id;
     var qtyList = []
     qtyList = req.query.qty;
 
-    //console.log(Object.keys(res.query));
+    /*
+    Second, some error handling.
+    -Check for an empty query.
+    -Then check to make sure the number of id params match the number of qty params.
+    */
 
     if(Object.keys(req.query).length === 0 ){
         res.type('html').status(200);
@@ -118,6 +156,16 @@ app.use('/calculatePrice', (req, res) =>{
         res.type('html').status(200);
         res.send({});
       } else {
+        /*
+        Certainly, there are more errors that should be handled, but per spec
+        this will suffice for testing purposes.
+
+        Now to build the query.
+        This will take the list of ids and run the query against MongoDB for
+        the toys.
+        We get what we get and will reconcile the results against the original
+        list as we tally up the bill.
+        */
         query = {id: {$in: idList}};
 
         Toy.find(query, (err, toys) =>{
@@ -126,10 +174,25 @@ app.use('/calculatePrice', (req, res) =>{
             res.send('Error: '+err);
           }
           else {
+            /*
+            Initializing a few variables that we'll be using throughout.
+            The 'bill', 'billIndex' and 'found' will be used for duplicate toys
+            passed into the query individually.
+            The 'total' probably only needs to be used at the end.
+            The 'deliverable' will be used at the end and will include the grand
+            total as well as the itemized subtotals.
+            */
             var bill = [];
             var total = 0;
             var found = false;
             var billIndex = 0;
+            var deliverable = {};
+            /*
+            We're checking three lists here:
+            -idList: This was build from the original query
+            -toys: Let's see which ones from our query were returned.
+            -bill: Update existing items on the bill, and add them if new.
+            */
             for(i = 0; i < idList.length; i++){
               for(j = 0 ; j < toys.length; j++){
                 if(idList[i] === toys[j].id && !isNaN(qtyList[i]) && qtyList[i] > 0){
@@ -161,7 +224,6 @@ app.use('/calculatePrice', (req, res) =>{
             for(i=0; i < bill.length; i++){
               total += bill[i].subtotal;
             }
-            var deliverable = {};
             deliverable.items = bill;
             deliverable.totalPrice = total;
             res.send(deliverable);
